4d41c594355a62201a3771c73039f5c8813781ef
kdr0324 <201211177@sangmyung.kr>
2019-05-02 13:27:13 +0900

    VR UI 에셋추가
    VR UI 에셋 임포트
    세이브 한 가구를 불러올 때 (clone)이름으로 인해 에러 나는 것 수정할 필요있음

---
+++ MyRoom/Assets/VRUI - Material Design/Scripts/VRUIRadio.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// VRUI Radio Button Component
+
+public class VRUIRadio : UnityEngine.UI.Toggle {
+
+	Color accentColor = Color.white;
+	Color outlineColor = Color.white;
+
+	Image outline;
+	Image dot;
+
+	protected override void Awake () {
+
+		// Override base method to add listener to set colors on value change
+
+		base.Awake ();
+
+		this.onValueChanged.AddListener(delegate {
+
+			SetRadioColor ();
+
+		});
+
+	}
+
+	public void setColors (Color a, Color b) {
+
+		// Initialize colors from the GUI
+
+		accentColor = a;
+	
+		outlineColor = b;
+
+		SetRadioColor ();
+	}
+
+
+	void SetRadioColor () 
+	{
+		// Assign the right color base on isOn state
+
+		if (outline == null)
+		{
+			outline = transform.Find ("Background/Outline").GetComponent<Image> ();
+		}
+			
+		if (dot == null)
+		{
+			dot = transform.Find ("Background/Dot").GetComponent<Image> ();
+		}
+
+		outline.CrossFadeColor(this.isOn ? accentColor : outlineColor, 0f, true, true);
+
+		dot.CrossFadeColor(this.isOn ? accentColor : new Color(1f, 1f, 1f, 0), 0f, true, true);
+	}
+}
